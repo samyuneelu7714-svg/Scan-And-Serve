e78632654c77e4c1770ecee5317f11be897a9b2d
Neelima Jyothi G <samyuneelu7714@gmail.com>
2026-07-26 17:16:22 +0530

    added  oreo milkshake recipe

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
-function generateBarcode() {
-
-    let food = document.getElementById("foodName").value.trim().toLowerCase();
-
-    if (food !== "mango" && food !== "apple" && food !== "banana") {
-        alert("Please enter Mango, Apple or Banana");
-        return;
-    }
-
-    JsBarcode("#barcode", food, {
-        format: "CODE128",
-        width: 2,
-        height: 100,
-        displayValue: true
-    });
-
+if (food === "mango") {
+    page = "mango.html";
 }
-
-function generateQR() {
-
-    let food = document.getElementById("foodName").value.trim().toLowerCase();
-
-    if (food !== "mango" && food !== "apple" && food !== "banana") {
-        alert("Please enter Mango, Apple or Banana");
-        return;
-    }
-
-    document.getElementById("qrcode").innerHTML = "";
-
-    const url = window.location.origin + "/recipe.html?fruit=" + food;
-
-    new QRCode(document.getElementById("qrcode"), {
-        text: url,
-        width: 220,
-        height: 220
-    });
-
+else if (food === "apple") {
+    page = "apple.html";
+}
+else if (food === "banana") {
+    page = "banana.html";
+}
+else if (food === "oreo") {
+    page = "oreo.html";
+}
+else {
+    alert("Please enter Mango, Apple, Banana or Oreo");
+    return;
 }
